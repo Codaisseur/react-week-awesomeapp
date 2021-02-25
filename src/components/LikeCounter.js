0ba7b8ce0c3c140db9e1f7d2bd16f0f4fbcf17d5
Domenico Gemoli <domenicogemoli@gmail.com>
2021-02-25 14:11:28 +0100

    Fix error from previous exercise

--- src/components/LikeCounter.js
+++ src/components/LikeCounter.js
@@ -7,13 +7,8 @@ export default function LikeCounter() {
 
   const increment = () => {
     console.log("Yes, clicked! Current number of likes:", numLikes);
+    set_numLikes(numLikes + 1);
   };
-
-  set_numLikes(numLikes + 1); // This causes an error!!
-
-  // This error is happening because the setter from useState causes a re-render,
-  // but we are calling the setter inside our main function, which is the function
-  // that React calls to render our component. In other words, infinite loop!!
 
   return (
     <div>
