eeba8de61c5a39d73c50def9666738b3a513160c
Domenico Gemoli <domenicogemoli@gmail.com>
2021-02-25 14:29:37 +0100

    Add axios and useEffect to ArticleList

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,31 +1,28 @@
 // src/components/ArticleList.js
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import ArticleCard from "./ArticleCard";
 
 export default function ArticleList() {
-  const [articles, set_articles] = useState([
-    {
-      id: 1,
-      title: "What is React all about?",
-      body:
-        "React is all about one-way data flow, the Virtual DOM, and transpiling JSX.",
-    },
-    {
-      id: 2,
-      title: "A lovely kid",
-      body: "In fact, a kid is also the name of a baby goat!",
-    },
-    {
-      id: 3,
-      title: "On placeholder image URLs",
-      body:
-        "So yeah, you won't be able to look these images up. They're placeholders",
-    },
-  ]);
+  const [articles, set_articles] = useState([]);
 
   const clearNotifications = () => {
     set_articles([]);
   };
+
+  useEffect(() => {
+    async function doSomeDataFetching() {
+      console.log("I'm gonna fetch some data!");
+
+      // Getting back data from the net, through the wire, air, and the ocean:
+      const res = await axios.get(
+        "https://jsonplaceholder.typicode.com/posts?_limit=5"
+      );
+
+      console.log("Got back:", res);
+    }
+    doSomeDataFetching();
+  }, []);
 
   return (
     <div>
