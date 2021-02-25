51b8d33c2015342b855c97e2ba2206542dbfce8e
Domenico Gemoli <domenicogemoli@gmail.com>
2021-02-25 14:17:50 +0100

    Get rid of pokemon

--- src/App.js
+++ src/App.js
@@ -1,40 +1,7 @@
 import "./App.css";
 import LikeButton from "./components/LikeButton";
 import LikeCounter from "./components/LikeCounter";
-import Pokemon from "./components/Pokemon";
-
 import Title from "./components/Title";
-
-const all_pokemon = [
-  {
-    name: "Charizard",
-    weight: 90,
-    awesome: true,
-    terrifying: false,
-    abilities: ["Blaze", "Solar power", "Tough claws", "Drought"],
-  },
-  {
-    name: "Bulbasaur",
-    weight: 6.9,
-    awesome: true,
-    terrifying: false,
-    abilities: ["Overgrow", "Chlorophyll"],
-  },
-  {
-    name: "Mewtwo",
-    weight: 122,
-    awesome: true,
-    terrifying: true,
-    abilities: ["Pressure", "Unnerve", "Steadfast", "Insomnia"],
-  },
-  {
-    name: "Mega beedrill",
-    weight: 65,
-    awesome: false,
-    terrifying: true,
-    abilities: ["Intimidate", "Unnerve"],
-  },
-];
 
 function App() {
   return (
@@ -42,19 +9,6 @@ function App() {
       <Title content="Some Simple Title" />
       <LikeCounter />
       <LikeButton />
-      <div className="row">
-        {all_pokemon.map((pokemon, index) => (
-          <div key={index} className="col-md-6 col-lg-4">
-            <Pokemon
-              name={pokemon.name}
-              weight={pokemon.weight}
-              awesome={pokemon.awesome}
-              terrifying={pokemon.terrifying}
-              abilities={pokemon.abilities}
-            />
-          </div>
-        ))}
-      </div>
     </main>
   );
 }
