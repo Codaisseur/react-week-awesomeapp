4d5ad6348cc1e704ef115c49becbdc9fd0b1128c
Domenico Gemoli <domenicogemoli@gmail.com>
2021-02-25 13:27:30 +0100

    Map over all_pokemon to render Pokemon

--- src/App.js
+++ src/App.js
@@ -3,50 +3,52 @@ import Pokemon from "./components/Pokemon";
 
 import Title from "./components/Title";
 
+const all_pokemon = [
+  {
+    name: "Charizard",
+    weight: 90,
+    awesome: true,
+    terrifying: false,
+    abilities: ["Blaze", "Solar power", "Tough claws", "Drought"],
+  },
+  {
+    name: "Bulbasaur",
+    weight: 6.9,
+    awesome: true,
+    terrifying: false,
+    abilities: ["Overgrow", "Chlorophyll"],
+  },
+  {
+    name: "Mewtwo",
+    weight: 122,
+    awesome: true,
+    terrifying: true,
+    abilities: ["Pressure", "Unnerve", "Steadfast", "Insomnia"],
+  },
+  {
+    name: "Mega beedrill",
+    weight: 65,
+    awesome: false,
+    terrifying: true,
+    abilities: ["Intimidate", "Unnerve"],
+  },
+];
+
 function App() {
-  const all_pokemon = [
-    { name: "Charizard", weight: 90 },
-    { name: "Bulbasaur", weight: 6.9 },
-    { name: "Mewtwo", weight: 122 },
-    { name: "Mega beedrill", weight: 65 },
-  ];
-
-  console.log(
-    all_pokemon.map(pokemon => `${pokemon.name}: ${pokemon.weight}kg`)
-  );
-
   return (
     <div className="App">
       <main>
         <Title content="Some Simple Title" />
-        <Pokemon
-          name="Charizard"
-          weight={90}
-          awesome={true}
-          terrifying={false}
-          abilities={["Blaze", "Solar power", "Tough claws", "Drought"]}
-        />
-        <Pokemon
-          name="Bulbasaur"
-          weight={6.9}
-          awesome={true}
-          terrifying={false}
-          abilities={["Overgrow", "Chlorophyll"]}
-        />
-        <Pokemon
-          name="Mewtwo"
-          weight={122}
-          awesome={true}
-          terrifying={true}
-          abilities={["Pressure", "Unnerve", "Steadfast", "Insomnia"]}
-        />
-        <Pokemon
-          name="Mega beedrill"
-          weight={65}
-          awesome={false}
-          terrifying={true}
-          abilities={["Intimidate", "Unnerve"]}
-        />
+        {all_pokemon.map((pokemon, index) => (
+          <Pokemon
+            key={index}
+            name={pokemon.name}
+            weight={pokemon.weight}
+            awesome={pokemon.awesome}
+            terrifying={pokemon.terrifying}
+            abilities={pokemon.abilities}
+          />
+        ))}
       </main>
     </div>
   );
